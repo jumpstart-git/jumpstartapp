e2bd725865954b300118786f04bab8e619ea979f
ASIAPACIFIC\chengca <carl-meyrick.cheng@hpe.com>
2017-04-07 17:35:40 +0800

    Additional fixes to Defect 005

--- studioactions/mobile/AS_RichText_d6ff08f069db47e1b086860d0827fe7b.js
+++ studioactions/mobile/AS_RichText_d6ff08f069db47e1b086860d0827fe7b.js
@@ -1,7 +1,9 @@
 function AS_RichText_d6ff08f069db47e1b086860d0827fe7b(eventobject, linktext, attributes, context) {
     kony.ui.Alert({
         "alertType": constants.ALERT_TYPE_CONFIRMATION,
-        "alertTitle": "Confirm",
+        //[D005] [Alerts] "Ensure all Alerts are categorized and updated - whether its:  - Action Required - Warning - Confirmation"(Testing completed)
+        "alertTitle": "Confirmation",
+        //END of D005
         "yesLabel": "Yes",
         "noLabel": "No",
         "message": "Do you really want to remove the brief?",
