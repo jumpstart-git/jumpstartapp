d295be9bdfef2714a86b94dbc4fbda86c6ab40db
ASIAPACIFIC\chengca <carl-meyrick.cheng@hpe.com>
2017-04-11 19:29:20 +0800

    D058 fixes

---
+++ studioactions/mobile/AS_Button_0f59a0f92f724438b5ea9a1898bd513d.js
@@ -0,0 +1,5 @@
+function AS_Button_0f59a0f92f724438b5ea9a1898bd513d(eventobject) {
+    kony.store.removeItem("imageVal");
+    kony.store.removeItem("imageFileNameVal");
+    registrationPage.show();
+}
